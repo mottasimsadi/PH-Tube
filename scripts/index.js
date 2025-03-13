eb9a05cf80532821670490f5b67f53e12c1e6457
mottasimsadi <mottasimsadi@gmail.com>
2025-03-13 15:07:58 +0600

    Completed designing the video card and display dynamic data

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,12 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
+}
+
 function displayCategories(categories) {
   // Get the container
   const categoryContainer = document.getElementById("category-container");
@@ -23,4 +29,41 @@ function displayCategories(categories) {
   }
 }
 
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videos.forEach((video) => {
+    // Create Element
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+        <div class="card bg-base-100">
+                <figure class="relative">
+                    <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" />
+                    <span class="absolute bottom-2 right-2 text-white text-sm bg-black px-2 rounded-full">3hrs 56 min
+                        ago</span>
+                </figure>
+                <div class="flex gap-3 px-0 py-5">
+                    <div class="profile">
+                        <div class="avatar">
+                            <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                                <img src="${video.authors[0].profile_picture}" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="intro">
+                        <h2 class="text-sm font-semibold">${video.title}</h2>
+                        <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}
+                            <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt="">
+                        </p>
+                        <p class="text-sm text-gray-400">${video.others.views} views</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    // Append the Element
+    videoContainer.appendChild(videoCard);
+  });
+};
+
 loadCategories();
+loadVideos();
