b9e7ca707950079f1bd536fd188267f7b0a8e884
mottasimsadi <mottasimsadi@gmail.com>
2025-03-14 18:46:46 +0600

    Completed the display video details and integrate modal function in main section

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 function removeActiveClass() {
   const activeButtons = document.getElementsByClassName("active");
-  
+
   for (let btn of activeButtons) {
     btn.classList.remove("active");
   }
@@ -24,6 +24,33 @@ function loadVideos() {
       displayVideos(data.videos);
     });
 }
+
+const loadVideoDetails = (videoId) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+
+const displayVideoDetails = (video) => {
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+
+  detailsContainer.innerHTML = `
+  <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+    <div class="card-actions justify-end">
+    </div>
+  </div>
+</div>
+  `;
+};
 
 const loadCategoryVideos = (id) => {
   const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
@@ -95,6 +122,7 @@ const displayVideos = (videos) => {
                         <p class="text-sm text-gray-400">${video.others.views} views</p>
                     </div>
                 </div>
+                <button onclick=loadVideoDetails("${video.video_id}") class="btn btn-block">Show Details</button>
             </div>
         `;
     // Append the Element
